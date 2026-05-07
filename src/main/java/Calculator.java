6ab1a7ca4797c760c074751632d10b8a22f275c1
Maxmak709 <115528533+Maxmak709@users.noreply.github.com>
2026-05-07 22:19:10 +0300

    Update Calculator.java

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,17 +1,22 @@
 public class Calculator {
     public int add(int a, int b){
-        //TODO inser your realisation in method add
+        return a + b;
     }
     public int dif(int a, int b){
-        //TODO inser your realisation in method dif
+        return a - b;
     }
     public int div(int a, int b){
-        //TODO inser your realisation in method div
+        if (b == 0) return 0; // Базовая проверка на ноль
+        return a / b;
     }
     public int times(int a, int b){
-        //TODO inser your realisation in method times
+        return a * b;
     }
+
     public int solver(){
-        //TODO inser your realisation in method solver
+        int step1 = add(10, 5);      // 15
+        int step2 = times(step1, 2);  // 30
+        int step3 = div(20, 4);      // 5
+        return dif(step2, step3);    // 25
     }
 }
